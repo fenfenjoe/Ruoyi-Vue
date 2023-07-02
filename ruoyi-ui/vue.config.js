1d44c2bcb6d52f9bb4315eda5bc2b814b1d5c066
azil <854257920@qq.com>
2023-07-02 22:06:41 +0800

    重新提交前端代码

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -35,7 +35,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:8080`, //后端接口IP地址+端口
+        target: process.env.BACKEND_IP_ADDRESS, //后端接口IP地址+端口
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
